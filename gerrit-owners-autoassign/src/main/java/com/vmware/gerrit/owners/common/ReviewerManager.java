0e1658d44f041ae193c53016112658213faae060
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-10-20 22:36:53 +0200

    Make plugin compile under master and new jgit dependencies
    Updated gitective to a more recent fork which is aligned with recent
    Jgit library.
    
    gerrit-owners folder updated for resolver.findAll to compile
    gerrit-owners-autoassign folder updated for interface changes in master
    gerrit-owners renamed to owners to match build rules
    
    Change-Id: Idded1c426353ec8499d3c690db82c4d4bc278c99

--- gerrit-owners-autoassign/src/main/java/com/vmware/gerrit/owners/common/ReviewerManager.java
+++ gerrit-owners-autoassign/src/main/java/com/vmware/gerrit/owners/common/ReviewerManager.java
@@ -3,70 +3,39 @@
  */
 package com.vmware.gerrit.owners.common;
 
-import com.google.gerrit.common.errors.EmailException;
-import com.google.gerrit.extensions.api.changes.AddReviewerInput;
+import com.google.gerrit.extensions.api.GerritApi;
+import com.google.gerrit.extensions.api.changes.ChangeApi;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
-import com.google.gerrit.server.CurrentUser;
-import com.google.gerrit.server.change.ChangeResource;
-import com.google.gerrit.server.change.ChangesCollection;
-import com.google.gerrit.server.change.PostReviewers;
-import com.google.gerrit.server.project.ChangeControl;
-import com.google.gerrit.server.project.NoSuchChangeException;
-import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
-import javax.inject.Inject;
-import javax.inject.Provider;
-import javax.inject.Singleton;
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Collection;
 
 @Singleton
 public class ReviewerManager {
+  private static final Logger log = LoggerFactory
+      .getLogger(ReviewerManager.class);
 
-  private final Provider<CurrentUser> currentUserProvider;
-
-  private final Provider<PostReviewers> postReviewersProvider;
-
-  private final ChangeControl.GenericFactory changeControlFactory;
-
-  private final ChangesCollection changesCollection;
-
+  private final GerritApi gApi;
   @Inject
-  public ReviewerManager(Provider<CurrentUser> currentUserProvider,
-                         Provider<PostReviewers> postReviewersProvider,
-                         ChangeControl.GenericFactory changeControlFactory,
-                         ChangesCollection changesCollection) {
-    this.currentUserProvider = currentUserProvider;
-    this.postReviewersProvider = postReviewersProvider;
-    this.changeControlFactory = changeControlFactory;
-    this.changesCollection = changesCollection;
+  public ReviewerManager(GerritApi gApi) {
+    this.gApi = gApi;
   }
-
   public void addReviewers(Change change, Collection<Account.Id> reviewers) throws ReviewerManagerException {
     try {
-      PostReviewers postReviewers = postReviewersProvider.get();
-      ChangeControl changeControl = changeControlFactory.controlFor(change, currentUserProvider.get());
-      ChangeResource changeResource = changesCollection.parse(changeControl);
-
-      // HACK(vspivak): Using PostReviewers is probably inefficient here, however it has all the hook/notification
-      // logic, so it's easier to call it then to mimic/copy the logic here.
-      for (Account.Id accountId : reviewers) {
-        AddReviewerInput input = new AddReviewerInput();
-        input.reviewer = accountId.toString();
-        postReviewers.apply(changeResource, input);
+      ChangeApi cApi = gApi.changes().id(change.getId().get());
+      for (Account.Id account : reviewers) {
+        cApi.addReviewer(account.toString());
       }
     } catch (RestApiException e) {
-      throw new ReviewerManagerException(e);
-    } catch (NoSuchChangeException e) {
-      throw new ReviewerManagerException(e);
-    } catch (EmailException e) {
-      throw new ReviewerManagerException(e);
-    } catch (OrmException e) {
-      throw new ReviewerManagerException(e);
-    } catch (IOException e) {
+      log.error("Couldn't add reviewers to the change", e);
       throw new ReviewerManagerException(e);
     }
   }
+
 }
