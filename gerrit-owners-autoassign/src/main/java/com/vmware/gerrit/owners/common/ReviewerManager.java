90644acb9c47b2384217bc46b6f571e490d60976
Luca Milanesio <luca.milanesio@gmail.com>
2016-10-27 12:18:26 +0100

    Set change owners execution context
    When adding reviewers to a change, set the current identified
    user to the local thread execution by impersonating the change
    owner.
    
    Change-Id: I2f1e1200b3d020483927e1d3803c52082fa468ec

--- gerrit-owners-autoassign/src/main/java/com/vmware/gerrit/owners/common/ReviewerManager.java
+++ gerrit-owners-autoassign/src/main/java/com/vmware/gerrit/owners/common/ReviewerManager.java
@@ -8,6 +8,9 @@ import com.google.gerrit.extensions.api.changes.ChangeApi;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.server.util.ManualRequestContext;
+import com.google.gerrit.server.util.OneOffRequestContext;
+import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -22,20 +25,26 @@ public class ReviewerManager {
       .getLogger(ReviewerManager.class);
 
   private final GerritApi gApi;
+  private final OneOffRequestContext requestContext;
+
   @Inject
-  public ReviewerManager(GerritApi gApi) {
+  public ReviewerManager(GerritApi gApi, OneOffRequestContext requestContext) {
     this.gApi = gApi;
+    this.requestContext = requestContext;
   }
-  public void addReviewers(Change change, Collection<Account.Id> reviewers) throws ReviewerManagerException {
-    try {
+
+  public void addReviewers(Change change, Collection<Account.Id> reviewers)
+      throws ReviewerManagerException {
+    try (ManualRequestContext ctx = requestContext.openAs(change.getOwner())) {
+
       ChangeApi cApi = gApi.changes().id(change.getId().get());
       for (Account.Id account : reviewers) {
         cApi.addReviewer(account.toString());
       }
-    } catch (RestApiException e) {
+
+    } catch (RestApiException | OrmException e) {
       log.error("Couldn't add reviewers to the change", e);
       throw new ReviewerManagerException(e);
     }
   }
-
 }
