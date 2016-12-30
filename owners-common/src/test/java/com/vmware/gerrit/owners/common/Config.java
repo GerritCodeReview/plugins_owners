7d14f9d4baaf1fb1166aeae5d8a6cc240f2f9c9c
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-12-30 10:07:34 +0100

    Matching based rules in OWNERS config
    Refactoring OWNERS file to be able to deal with matchers. Syntax has been
    enhanced to be able to specify inherited matches blocks that can be
    queried from rules.pl to understand if a change can be submitted based on
    regular expression, exact match or suffix matching.
    
    An example of the new OWNERS syntax:
    
    inherits: false
    owners:
    - email1
    - email2
    matches:
    - regex: .*/a*
      owners:
      - email3
      - email4
    - exact: Product.java
      owners:
      - email5
      - email6
    - suffix: .java
      owners:
      - email7
      - email8
    
    A possible rules.pl exploiting this can be written as follows:
    In this case we impose that for every path defined by a matchers block
    at least one of the associated owners must give a +2 to have the change
    submittable
    
    % default rule
    submit_rule(S) :-
      gerrit:default_submit(S).
    
    % add extra label for every path P
    submit_rule(submit(W)) :-
            gerrit_owners:matcher_path(P),
            findall(US,code_review_user(US),Approvers),
            matcher_needed(Approvers,P,W).
    
    % to get only matchers for that path
    matcher_needed(Users,Path,W) :-
       findall(US,needed_review_user(Path,US),NSL),
       subtract(Users,NSL,Diff),
       % if Users - Needed is unchanged this means we are missing at
       % least one (!)
       Diff == Users,
       W = label(Path, need(_)).
    
    code_review_user(US) :-
       gerrit:commit_label(label('Code-Review', 2), user(U)),
       user_string(U,US).
    
    needed_review_user(Path,US) :-
       gerrit_owners:matcher_owner(Path,user(User)),
       user_string(User,US).
    
    Change-Id: If5a389ad696bdec236f68af4bf1ee65f1d6d04fa

--- owners-common/src/test/java/com/vmware/gerrit/owners/common/Config.java
+++ owners-common/src/test/java/com/vmware/gerrit/owners/common/Config.java
@@ -1,31 +1,44 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.vmware.gerrit.owners.common;
 
 import static org.easymock.EasyMock.anyObject;
 import static org.easymock.EasyMock.eq;
 import static org.easymock.EasyMock.expect;
-import static org.powermock.api.easymock.PowerMock.replayAll;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.Maps;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Patch;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.account.AccountResolver;
-import com.google.gerrit.server.patch.ComparisonType;
 import com.google.gerrit.server.patch.PatchList;
 import com.google.gerrit.server.patch.PatchListEntry;
 import com.google.gwtorm.server.OrmException;
 
-import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.junit.Ignore;
 import org.powermock.api.easymock.PowerMock;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 
@@ -44,7 +57,7 @@ public abstract class Config {
     db = PowerMock.createMock(ReviewDb.class);
     repository = PowerMock.createMock(Repository.class);
     resolver = PowerMock.createMock(AccountResolver.class);
-    parser = new ConfigurationParser();
+    parser = new ConfigurationParser(resolver, db);
     resolvingEmailToAccountIdMocking();
   }
 
@@ -67,18 +80,14 @@ public abstract class Config {
     accounts.put(email, acct);
   }
 
-  void creatingPatchList(String name, String name2) {
+  void creatingPatchList(List<String> names) {
     patchList = PowerMock.createMock(PatchList.class);
-    PatchListEntry entry = expectEntry(name);
-    if (name2 != null) {
-      PatchListEntry entry2 = expectEntry(name2);
-      expect(patchList.getPatches()).andReturn(Arrays.asList(entry,entry2));
-    } else {
-      expect(patchList.getPatches()).andReturn(Arrays.asList(entry));
-    }
+    List<PatchListEntry> entries = names.stream()
+        .map(name -> expectEntry(name)).collect(Collectors.toList());
+    expect(patchList.getPatches()).andReturn(entries);
   }
-  PatchListEntry expectEntry(String name) {
 
+  PatchListEntry expectEntry(String name) {
     PatchListEntry entry = PowerMock.createMock(PatchListEntry.class);
     expect(entry.getNewName()).andReturn(name).anyTimes();
     expect(entry.getChangeType()).andReturn(Patch.ChangeType.MODIFIED)
@@ -86,5 +95,9 @@ public abstract class Config {
     expect(entry.getDeletions()).andReturn(1);
     expect(entry.getInsertions()).andReturn(1);
     return entry;
+  }
+
+  Optional<OwnersConfig> getOwnersConfig(String string) {
+    return parser.getOwnersConfig(string.getBytes(Charsets.UTF_8));
   }
 }
---
+++ owners/src/main/java/gerrit_owners/PRED_matcher_owner_2.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2013 VMware, Inc. All Rights Reserved.
+ */
+package gerrit_owners;
+
+import com.vmware.gerrit.owners.OwnersStoredValues;
+import com.vmware.gerrit.owners.common.Matcher;
+import com.vmware.gerrit.owners.common.PathOwners;
+
+import com.google.gerrit.reviewdb.client.Account;
+import com.googlecode.prolog_cafe.exceptions.PrologException;
+import com.googlecode.prolog_cafe.lang.IntegerTerm;
+import com.googlecode.prolog_cafe.lang.JavaObjectTerm;
+import com.googlecode.prolog_cafe.lang.Operation;
+import com.googlecode.prolog_cafe.lang.Predicate;
+import com.googlecode.prolog_cafe.lang.Prolog;
+import com.googlecode.prolog_cafe.lang.StructureTerm;
+import com.googlecode.prolog_cafe.lang.SymbolTerm;
+import com.googlecode.prolog_cafe.lang.Term;
+
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * 'owner'(-Path, -User)
+ */
+public class PRED_matcher_owner_2 extends Predicate.P2 {
+
+  private static final PRED_owner_check OWNER_CHECK = new PRED_owner_check();
+  private static final PRED_owner_empty OWNER_EMPTY = new PRED_owner_empty();
+  private static final PRED_owner_next OWNER_NEXT = new PRED_owner_next();
+
+  public PRED_matcher_owner_2(Term a1, Term a2, Operation n) {
+    this.arg1 = a1;
+    this.arg2 = a2;
+    this.cont = n;
+  }
+
+  @Override
+  public Operation exec(Prolog engine) throws PrologException {
+    engine.cont = cont;
+    engine.setB0();
+
+    PathOwners owners = OwnersStoredValues.PATH_OWNERS.get(engine);
+    engine.r1 = arg1;
+    engine.r2 = arg2;
+
+    Term term = engine.r1.dereference();
+    String path = term.toString();
+
+    engine.r3 = new JavaObjectTerm(owners.getMatches().get(path).getOwners().iterator());
+    return engine.jtry3(OWNER_CHECK, OWNER_NEXT);
+  }
+
+  private static class PRED_owner_check extends Operation {
+
+    @Override
+    public Operation exec(Prolog engine) throws PrologException {
+      Term a1 = engine.r1;
+      Term a2 = engine.r2;
+      Term a3 = engine.r3;
+
+      Term term = engine.r1.dereference();
+      String pathS = term.toString();
+
+      @SuppressWarnings("unchecked")
+      Iterator<Account.Id> iter = (Iterator<Account.Id>) ((JavaObjectTerm) a3).object();
+      while (iter.hasNext()) {
+        Account.Id entry = iter.next();
+
+        SymbolTerm path = SymbolTerm.create(pathS);
+        if (!a1.unify(path, engine.trail)) {
+          continue;
+        }
+
+        StructureTerm user = new StructureTerm("user", new IntegerTerm(entry.get()));
+        if (!a2.unify(user, engine.trail)) {
+          continue;
+        }
+
+        return engine.cont;
+      }
+      return engine.fail();
+    }
+  }
+
+  private static class PRED_owner_next extends Operation {
+
+    @Override
+    public Operation exec(Prolog engine) throws PrologException {
+      return engine.trust(OWNER_EMPTY);
+    }
+  }
+
+  private static class PRED_owner_empty extends Operation {
+
+    @Override
+    public Operation exec(Prolog engine) throws PrologException {
+      Term a3 = engine.r3;
+
+      @SuppressWarnings("unchecked")
+      Iterator<Map.Entry<String, Account.Id>> iter =
+          (Iterator<Map.Entry<String, Account.Id>>) ((JavaObjectTerm) a3).object();
+      if (!iter.hasNext()) {
+        return engine.fail();
+      }
+
+      return engine.jtry3(OWNER_CHECK, OWNER_NEXT);
+    }
+  }
+
+}
