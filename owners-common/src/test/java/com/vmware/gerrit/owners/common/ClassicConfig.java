89469049d8b2b943c8e25167d5dbc3771fa35e32
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-12-30 10:07:34 +0100

    [WIP] Matching based rules in OWNERS config
    Refactoring OWNERS file to be able to deal with matchers. Syntax has been
    enhanced to be able to specify inherited matches blocks that can be
    queried from rules.pl to understand if a change can be submitted based on
    regular expression, exact match or suffix matching.
    
    An example of the new OWNERS syntax:
    
    inherits: false
    owners:
    - email1
    - email2
    matches:
    - regex: .*/a*
      owners:
      - email3
      - email4
    - exact: Product.java
      owners:
      - email5
      - email6
    - suffix: .java
      owners:
      - email7
      - email8
    
    A possible rules.pl exploiting this can be written as follows:
    In this case we impose that for every path defined by a matchers block
    at least one of the associated owners must give a +2 to have the change
    submittable
    
    % default rule
    submit_rule(S) :-
      gerrit:default_submit(S).
    
    % add extra label for every path P
    submit_rule(submit(W)) :-
            gerrit_owners:matcher_path(P),
            findall(US,code_review_user(US),Approvers),
            matcher_needed(Approvers,P,W).
    
    % to get only matchers for that path
    matcher_needed(Users,Path,W) :-
       findall(US,needed_review_user(Path,US),NSL),
       subtract(Users,NSL,Diff),
       % if Users - Needed is unchanged this means we are missing at
       % least one (!)
       Diff == Users,
       W = label(Path, need(_)).
    
    code_review_user(US) :-
       gerrit:commit_label(label('Code-Review', 2), user(U)),
       user_string(U,US).
    
    needed_review_user(Path,US) :-
       gerrit_owners:matcher_owner(Path,user(User)),
       user_string(User,US).
    
    Change-Id: If5a389ad696bdec236f68af4bf1ee65f1d6d04fa

--- owners-common/src/test/java/com/vmware/gerrit/owners/common/ClassicConfig.java
+++ owners-common/src/test/java/com/vmware/gerrit/owners/common/ClassicConfig.java
@@ -1,10 +1,5 @@
 package com.vmware.gerrit.owners.common;
 
-import static org.powermock.api.easymock.PowerMock.replayAll;
-
-import com.google.common.base.Charsets;
-import com.google.gwtorm.server.OrmException;
-
 import java.nio.charset.Charset;
 import java.util.Optional;
 
@@ -14,58 +9,49 @@ public class ClassicConfig extends Config {
   protected static final int USERB = 2;
   protected static final int USERC = 3;
 
-
-  void configurationClassic(boolean inherit) throws Exception {
-    expectWrapper("OWNERS",Optional.empty());
-    if (inherit) {
-      expectWrapper("/OWNERS",Optional.of(
-          ("inherited: true\n" +
-                      "owners:\n" +
-                      "- user-c@email.com").getBytes(Charset.defaultCharset())));
-    } else {
-      expectWrapper("/OWNERS",Optional.empty());
-    }
-
-    expectWrapper("/classic/OWNERS", Optional.of(
-        ("inherited: true\n" +
-                    "owners:\n" +
-                    "- user-a@email.com\n" +
-                    "- user-b@email.com").getBytes(Charset.defaultCharset())));
-    creatingPatchList("/classic/file.txt", null);
-    replayAll();
+  void configurationClassic() throws Exception {
+    expectWrapper("OWNERS", Optional.empty());
+    expectWrapper("classic/OWNERS",
+        Optional.of(("inherited: true\n" + "owners:\n"
+            + "- user-a@email.com\n" + "- user-b@email.com")
+                .getBytes(Charset.defaultCharset())));
 
   }
+
+  void configurationClassicInherited() throws Exception {
+    expectWrapper("OWNERS",
+        Optional.of(
+            ("inherited: true\n" + "owners:\n" + "- user-c@email.com")
+                .getBytes(Charset.defaultCharset())));
+
+    expectWrapper("classic/OWNERS",
+        Optional.of(("inherited: true\n" + "owners:\n"
+            + "- user-a@email.com\n" + "- user-b@email.com")
+                .getBytes(Charset.defaultCharset())));
+
+  }
+
   void configurationClassicBis() throws Exception {
-    expectWrapper("OWNERS",Optional.empty());
-    String value = "inherited: false\n" +
-    "owners:\n"+
-    "- user-a@email.com";
+    String value =
+        "inherited: false\n" + "owners:\n" + "- user-a@email.com";
 
-    expectWrapper("/OWNERS",Optional.of(value.getBytes()));
+    expectWrapper("OWNERS", Optional.of(value.getBytes()));
 
-    String project = "inherited: true\n"+
-    "owners:\n"+
-    "- user-b@email.com\n" +
-    "- user-c@email.com";
-    expectWrapper("/project/OWNERS",Optional.of(project.getBytes()));
-    String sql = "inherited: true\n" +
-    "owners:\n" +
-    "- user-b@email.com";
-    expectWrapper("/sql/OWNERS",Optional.of(sql.getBytes()));
-    creatingPatchList("/sql/file1","/project/fileA.txt");
-    replayAll();
+    String project = "inherited: true\n" + "owners:\n"
+        + "- user-b@email.com\n" + "- user-c@email.com";
+
+    expectWrapper("project/OWNERS", Optional.of(project.getBytes()));
+    String sql =
+        "inherited: true\n" + "owners:\n" + "- user-b@email.com";
+    expectWrapper("sql/OWNERS", Optional.of(sql.getBytes()));
+
   }
 
   @Override
   public void resolvingEmailToAccountIdMocking() throws Exception {
-
-    resolveEmail("user-a@email.com",USERA);
-    resolveEmail("user-b@email.com",USERB);
-    resolveEmail("user-c@email.com",USERC);
+    resolveEmail("user-a@email.com", USERA);
+    resolveEmail("user-b@email.com", USERB);
+    resolveEmail("user-c@email.com", USERC);
 
   }
-
-
-
-
 }
