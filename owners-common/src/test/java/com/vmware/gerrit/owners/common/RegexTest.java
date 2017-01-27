e58a22300551edfce8001f160938c25b94b2f268
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2017-01-27 02:52:10 +0100

    FileOwners added and UnitTested
    Change-Id: I4de7bac2030474e51f982c61611b7d0dc14ab5a8

--- owners-common/src/test/java/com/vmware/gerrit/owners/common/RegexTest.java
+++ owners-common/src/test/java/com/vmware/gerrit/owners/common/RegexTest.java
@@ -14,10 +14,10 @@
 
 package com.vmware.gerrit.owners.common;
 
+import static com.vmware.gerrit.owners.common.StreamUtils.iteratorStream;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.powermock.api.easymock.PowerMock.replayAll;
-import static com.vmware.gerrit.owners.common.StreamUtils.iteratorStream;
 
 import com.google.gerrit.reviewdb.client.Account;
 
@@ -48,57 +48,84 @@ public class RegexTest extends RegexConfig {
   @Test
   public void testNewParsingYaml() throws Exception {
     replayAll();
-    String yamlString = ADVANCED_CONFIG_FULL;
+
+    String fullConfig =
+        createConfig(true, new String[] {"a"}, new Matcher[] {
+            new SuffixMatcher(".sql",
+                convertEmailsToIds(new String[] {"b", "c"})),
+            new RegExMatcher(".*/a.*", convertEmailsToIds(new String[] {"d"}))});
     // the function to test
-    Optional<OwnersConfig> configNullable = getOwnersConfig(yamlString);
+    Optional<OwnersConfig> configNullable = getOwnersConfig(fullConfig);
     // check classical configuration
     assertTrue(configNullable.isPresent());
+
     OwnersConfig config = configNullable.get();
+    assertTrue(config.isInherited());
+
     Set<String> owners = config.getOwners();
-    assertEquals(2, owners.size());
-    assertTrue(owners.contains("jane@email.com"));
-    assertTrue(owners.contains("john@email.com"));
+    assertEquals(1, owners.size());
+    assertTrue(owners.contains("a"));
     // check matchers
     Map<String, Matcher> matchers = config.getMatchers();
     assertEquals(2, matchers.size());
     assertTrue(matchers.containsKey(".sql"));
-    assertTrue(matchers.containsKey("Product.scala"));
-    Matcher advMatcher = matchers.get("Product.scala");
+    assertTrue(matchers.containsKey(".*/a.*"));
+
+    // suffix .sql matcher
+    Matcher advMatcher = matchers.get(".sql");
     assertEquals(2, advMatcher.getOwners().size());
     Set<Account.Id> advOwners = advMatcher.getOwners();
-    assertTrue(advOwners.contains(accounts.get("bob@email.com")));
-    assertTrue(advOwners.contains(accounts.get("alice@email.com")));
-    Matcher dbMatcher = matchers.get(".sql");
-    assertEquals(2, dbMatcher.getOwners().size());
+    assertTrue(advOwners.contains(mapping.inverse().get("b")));
+    assertTrue(advOwners.contains(mapping.inverse().get("c")));
+
+    // regex matcher
+    Matcher dbMatcher = matchers.get(".*/a.*");
+    assertEquals(1, dbMatcher.getOwners().size());
     Set<Account.Id> dbOwners = dbMatcher.getOwners();
-    assertTrue(dbOwners.contains(accounts.get("philip@email.com")));
-    assertTrue(dbOwners.contains(accounts.get("frank@email.com")));
+    assertTrue(dbOwners.contains(mapping.inverse().get("d")));
   }
 
   @Test
   public void checkMatchers() throws Exception {
-    expectWrapper("OWNERS",
-        Optional.of(ADVANCED_PARENT_CONFIG.getBytes()));
-    expectWrapper("project/OWNERS",
-        Optional.of(ADVANCED_CONFIG.getBytes()));
-    creatingPatchList(Arrays.asList("file1.txt", "project/afile2.txt",
-        "project/bfile.txt"));
+
+    String parentConfig =
+        createConfig(true, new String[] {"a"}, new Matcher[] {
+            new SuffixMatcher(".sql",
+                convertEmailsToIds(new String[] {"b", "c"})),
+            new RegExMatcher(".*/a.*", convertEmailsToIds(new String[] {"d"}))});
+    String childConfig =
+        createConfig(true, new String[] {"f"}, new Matcher[] {new ExactMatcher(
+            "project/file.txt", convertEmailsToIds(new String[] {"d", "e"}))});
+
+    expectWrapper("OWNERS", Optional.of(parentConfig.getBytes()));
+    expectWrapper("project/OWNERS", Optional.of(childConfig.getBytes()));
+
+    creatingPatchList(Arrays.asList("project/file.txt", // matches exact in
+                                                        // project owners d,e
+        "file1.txt", // no matches so nothing for this
+        "project/afile2.sql", // matches two matchers so we have b,c,d
+        "project/bfile.txt", // no matching
+        "project/file.sql")); // only .sql matching b,c
     replayAll();
+
     // function under test
-    PathOwners owners =
-        new PathOwners(resolver, db, repository, patchList);
+    PathOwners owners = new PathOwners(resolver, db, repository, patchList);
+
     // assertions on classic owners
     Set<Account.Id> ownersSet = owners.get().get("project/OWNERS");
     assertEquals(2, ownersSet.size());
+
     // get matches
     Map<String, Matcher> matches = owners.getMatches();
+    assertEquals(3, matches.size());
+
     // asserts we have 1 exact matcher
     List<Entry<String, Matcher>> onlyExacts =
-        iteratorStream(matches.entrySet().iterator())
-            .filter(entry -> entry.getValue() instanceof ExactMatcher)
-            .collect(Collectors.toList());
+        iteratorStream(matches.entrySet().iterator()).filter(
+            entry -> entry.getValue() instanceof ExactMatcher).collect(
+            Collectors.toList());
     assertEquals(1, onlyExacts.size());
-    assertEquals("file1.txt", onlyExacts.get(0).getKey());
+    assertEquals("project/file.txt", onlyExacts.get(0).getKey());
     // ... 1 regex matcher
     List<Entry<String, Matcher>> regexList =
         StreamUtils.iteratorStream(matches.entrySet().iterator())
@@ -106,16 +133,33 @@ public class RegexTest extends RegexConfig {
             .collect(Collectors.toList());
     assertEquals(1, regexList.size());
     assertEquals(".*/a.*", regexList.get(0).getKey());
-    // ... and not other matchers
-    assertEquals(2, matches.size());
-    matches.forEach((key, value) -> {
-      final StringBuffer buf = new StringBuffer("key:" + key + "\n"
-          + "path:" + value.getPath() + "\n" + "owners: ");
-      value.getOwners().forEach(account -> {
-        buf.append(account.toString() + " ");
-      });
-      buf.append("\n");
-      System.out.println(buf);
-    });
+
+    // .... 1 suffix matcher
+    List<Entry<String, Matcher>> suffixList =
+        iteratorStream(matches.entrySet().iterator()).filter(
+            entry -> entry.getValue() instanceof SuffixMatcher).collect(
+            Collectors.toList());
+    assertEquals(1, suffixList.size());
+    assertEquals(".sql", suffixList.get(0).getKey());
+
+    // now checks file owners as well
+    Map<String, Set<Account.Id>> fileOwners = owners.getFileOwners();
+    assertEquals(3, fileOwners.size());
+
+    Set<Account.Id> set1 = fileOwners.get("project/file.txt");
+    assertEquals(2, set1.size());
+    assertTrue(set1.contains(mapping.inverse().get("d")));
+    assertTrue(set1.contains(mapping.inverse().get("e")));
+
+    Set<Account.Id> set2 = fileOwners.get("project/afile2.sql");
+    assertEquals(3, set2.size());
+    assertTrue(set2.contains(mapping.inverse().get("b")));
+    assertTrue(set2.contains(mapping.inverse().get("c")));
+    assertTrue(set2.contains(mapping.inverse().get("d")));
+
+    Set<Account.Id> set3 = fileOwners.get("project/file.sql");
+    assertEquals(2, set3.size());
+    assertTrue(set3.contains(mapping.inverse().get("b")));
+    assertTrue(set3.contains(mapping.inverse().get("c")));
   }
 }
