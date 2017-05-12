3c431bf484d7b16f808f6c7c3b15ef9804c07c6d
Luca Milanesio <luca.milanesio@gmail.com>
2017-05-12 17:25:59 +0100

    Allow any external id to be matched as owner
    This fixes the regression introduced with 189a37fb when adapting
    to the Gerrit v2.14 and master API.
    Previously the "schemeRest" was matched, and that needs to continue
    to be the case.
    
    Allows to specify either a username, e-mail or any other external id
    in the OWNERS file.
    
    Change-Id: I29c70ff657530fe05313731aac10345b95e8fa3a

--- owners-common/src/main/java/com/vmware/gerrit/owners/common/AccountsImpl.java
+++ owners-common/src/main/java/com/vmware/gerrit/owners/common/AccountsImpl.java
@@ -39,8 +39,7 @@ public class AccountsImpl implements Accounts {
   private final AccountCache byId;
 
   @Inject
-  public AccountsImpl(AccountResolver resolver, AccountCache byId,
-      ReviewDb db) {
+  public AccountsImpl(AccountResolver resolver, AccountCache byId, ReviewDb db) {
     this.resolver = resolver;
     this.byId = byId;
     this.db = db;
@@ -48,25 +47,45 @@ public class AccountsImpl implements Accounts {
 
   @Override
   public Set<Account.Id> find(String nameOrEmail) {
-    Set<Id> accountIds = Collections.emptySet();
     try {
-      accountIds = resolver.findAll(db, nameOrEmail).stream()
-          .filter(id -> isFullMatch(id, nameOrEmail))
-          .collect(Collectors.toSet());
+      Set<Id> accountIds = resolver.findAll(db, nameOrEmail);
+      if (accountIds.isEmpty()) {
+        log.warn("User '{}' does not resolve to any account.", nameOrEmail);
+        return accountIds;
+      }
+
+      Set<Id> fulllyMatchedAccountIds =
+          accountIds
+              .stream()
+              .filter(id -> isFullMatch(id, nameOrEmail))
+              .collect(Collectors.toSet());
+      if (fulllyMatchedAccountIds.isEmpty()) {
+        log.warn(
+            "User '{}' resolves to {} accounts {}, but does not correspond to any them",
+            nameOrEmail,
+            accountIds.size(),
+            accountIds);
+        return fulllyMatchedAccountIds;
+      }
+
+      return accountIds;
     } catch (OrmException e) {
       log.error("Error trying to resolve user " + nameOrEmail, e);
+      return Collections.emptySet();
     }
-    if (accountIds.isEmpty()) {
-      log.warn("User {} does not correspond to any account id", nameOrEmail);
-    }
-    return accountIds;
   }
 
   private boolean isFullMatch(Account.Id id, String nameOrEmail) {
     AccountState account = byId.get(id);
-    return account.getAccount().getFullName().trim()
-        .equalsIgnoreCase(nameOrEmail)
-        || account.getExternalIds().stream().anyMatch(extId -> extId
-            .key().get().trim().equalsIgnoreCase(ExternalId.SCHEME_MAILTO + ":" + nameOrEmail));
+    return account.getAccount().getFullName().trim().equalsIgnoreCase(nameOrEmail)
+        || account
+            .getExternalIds()
+            .stream()
+            .anyMatch(extId -> getSchemeRest(extId.key()).trim().equalsIgnoreCase(nameOrEmail));
+  }
+
+  private String getSchemeRest(ExternalId.Key key) {
+    String scheme = key.scheme();
+    return null != scheme ? key.get().substring(scheme.length() + 1) : null;
   }
 }
