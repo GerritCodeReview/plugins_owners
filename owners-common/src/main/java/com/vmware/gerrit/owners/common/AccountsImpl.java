GerritCodeReview/plugins_owners

commit 189a37fb01fc03e61b1bd60abbc57ab9eb42aa58

Fix external id on Gerrit master and stable-2.14
External ids have been refactored on master and stable-2.14.
Adapt the code to be more version-agnostic and work with a wider
set of Gerrit versions.

Change-Id: I5a481c31e3fc1cd140dcca5cad92696d7930b074

owners-common/src/main/java/com/vmware/gerrit/owners/common/AccountsImpl.java
@@ -20,6 +20,7 @@ import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.account.AccountCache;
 import com.google.gerrit.server.account.AccountResolver;
 import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.account.ExternalId;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 
@@ -66,6 +67,6 @@ public class AccountsImpl implements Accounts {
     return account.getAccount().getFullName().trim()
         .equalsIgnoreCase(nameOrEmail)
         || account.getExternalIds().stream().anyMatch(extId -> extId
-            .getSchemeRest().trim().equalsIgnoreCase(nameOrEmail));
+            .key().get().trim().equalsIgnoreCase(ExternalId.SCHEME_MAILTO + ":" + nameOrEmail));
   }
 }

commit 6e85543f495ac2883d34b1be7ee4e208ef35eeb6

Allow any external id to be matched as owner
This fixes the regression introduced with 189a37fb when adapting
to the Gerrit v2.14 and master API.
Previously the "schemeRest" was matched, and that needs to continue
to be the case.

Allows to specify either a username, e-mail or any other external id
in the OWNERS file.

Change-Id: I29c70ff657530fe05313731aac10345b95e8fa3a

owners-common/src/main/java/com/vmware/gerrit/owners/common/AccountsImpl.java
@@ -20,7 +20,6 @@ import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.account.AccountCache;
 import com.google.gerrit.server.account.AccountResolver;
 import com.google.gerrit.server.account.AccountState;
-import com.google.gerrit.server.account.ExternalId;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 
@@ -64,9 +63,15 @@ public class AccountsImpl implements Accounts {
 
   private boolean isFullMatch(Account.Id id, String nameOrEmail) {
     AccountState account = byId.get(id);
-    return account.getAccount().getFullName().trim()
-        .equalsIgnoreCase(nameOrEmail)
-        || account.getExternalIds().stream().anyMatch(extId -> extId
-            .key().get().trim().equalsIgnoreCase(ExternalId.SCHEME_MAILTO + ":" + nameOrEmail));
+    return account.getAccount().getFullName().trim().equalsIgnoreCase(nameOrEmail)
+        || account
+            .getExternalIds()
+            .stream()
+            .anyMatch(extId -> getSchemeRest(extId.key().get()).equalsIgnoreCase(nameOrEmail));
+  }
+
+  private String getSchemeRest(String extIdString) {
+    int colonPos = extIdString.indexOf(':');
+    return extIdString.substring(colonPos + 1);
   }
 }