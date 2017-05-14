18e1a1a17d7f6d3645d588e798853cad0c830562
Luca Milanesio <luca.milanesio@gmail.com>
2017-05-14 23:35:29 +0100

    Use Gerrit v2.14 native one-off request context
    No need anymore to use the copied Gerrit214xx class for the propagation
    of a custom user context.
    
    Change-Id: Idc01aedd228d18eae6ceee9f03b8b64726e8cea0

--- owners-autoassign/src/main/java/com/vmware/gerrit/owners/common/ReviewerManager.java
+++ owners-autoassign/src/main/java/com/vmware/gerrit/owners/common/ReviewerManager.java
@@ -21,6 +21,8 @@ import com.google.gerrit.extensions.api.changes.ChangeApi;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.server.util.ManualRequestContext;
+import com.google.gerrit.server.util.OneOffRequestContext;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -36,18 +38,18 @@ public class ReviewerManager {
       .getLogger(ReviewerManager.class);
 
   private final GerritApi gApi;
-  private final Gerrit214OneOffRequestContext requestContext;
+  private final OneOffRequestContext requestContext;
 
   @Inject
   public ReviewerManager(GerritApi gApi,
-      Gerrit214OneOffRequestContext requestContext) {
+      OneOffRequestContext requestContext) {
     this.gApi = gApi;
     this.requestContext = requestContext;
   }
 
   public void addReviewers(Change change, Collection<Account.Id> reviewers)
       throws ReviewerManagerException {
-    try (Gerrit214ManualRequestContext ctx = requestContext.openAs(
+    try (ManualRequestContext ctx = requestContext.openAs(
       change.getOwner())) {
 
       ChangeApi cApi = gApi.changes().id(change.getId().get());
