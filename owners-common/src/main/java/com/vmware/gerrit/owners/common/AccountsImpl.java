421167ce9f96bc1347f1061a758e11087995e5d1
Luca Milanesio <luca.milanesio@gmail.com>
2017-05-14 22:43:59 +0100

    Allow to define group owners using group/ prefix
    When in the OWNERS file the group/ prefix is used, the Prolog rule
    to extract the owners will list all the members of the group specified
    as either group name or UUID.
    
    Example:
    
    inherited: true
    owners:
    - group/DBA
    
    where DBA is a Gerrit group representing the team of DB administrators.
    
    Change-Id: I7562b73180979f8301cc7bd012dc83822df948d4

--- owners-common/src/main/java/com/vmware/gerrit/owners/common/AccountsImpl.java
+++ owners-common/src/main/java/com/vmware/gerrit/owners/common/AccountsImpl.java
@@ -14,38 +14,92 @@
 
 package com.vmware.gerrit.owners.common;
 
+import com.google.gerrit.common.errors.NoSuchGroupException;
 import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.AccountGroup;
 import com.google.gerrit.reviewdb.client.Account.Id;
 import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.account.AccountCache;
 import com.google.gerrit.server.account.AccountResolver;
 import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.account.GroupCache;
+import com.google.gerrit.server.account.GroupMembers;
+import com.google.gerrit.server.project.NoSuchProjectException;
+import com.google.gerrit.server.util.ManualRequestContext;
+import com.google.gerrit.server.util.OneOffRequestContext;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class AccountsImpl implements Accounts {
   private static final Logger log = LoggerFactory.getLogger(AccountsImpl.class);
 
-  private ReviewDb db;
-  private AccountResolver resolver;
+  private final ReviewDb db;
+  private final AccountResolver resolver;
   private final AccountCache byId;
+  private final GroupCache groupCache;
+  private final GroupMembers.Factory groupMembers;
+  private final IdentifiedUser adminUser;
+  private final OneOffRequestContext oneOffRequestContext;
 
   @Inject
-  public AccountsImpl(AccountResolver resolver, AccountCache byId, ReviewDb db) {
+  public AccountsImpl(
+      AccountResolver resolver,
+      AccountCache byId,
+      ReviewDb db,
+      GroupCache groupCache,
+      GroupMembers.Factory groupMembersFactory,
+      OneOffRequestContext oneOffRequestContext,
+      IdentifiedUser.GenericFactory userFactory) {
     this.resolver = resolver;
     this.byId = byId;
     this.db = db;
+    this.groupCache = groupCache;
+    this.groupMembers = groupMembersFactory;
+    this.adminUser = userFactory.create(new Account.Id(1000000));
+    this.oneOffRequestContext = oneOffRequestContext;
   }
 
   @Override
-  public Set<Account.Id> find(String nameOrEmail) {
+  public Set<Account.Id> find(String nameOrEmailOrGroup) {
+    if (nameOrEmailOrGroup.startsWith(GROUP_PREFIX)) {
+      return findAccountsInGroup(nameOrEmailOrGroup.substring(GROUP_PREFIX.length()));
+    }
+    return findUserOrEmail(nameOrEmailOrGroup);
+  }
+
+  private Set<Id> findAccountsInGroup(String groupNameOrUUID) {
+    AccountGroup group =
+        Optional.ofNullable(groupCache.get(new AccountGroup.NameKey(groupNameOrUUID)))
+            .orElse(groupCache.get(new AccountGroup.UUID(groupNameOrUUID)));
+
+    if (group == null) {
+      log.warn("Group {} was not found", groupNameOrUUID);
+    }
+
+    try (ManualRequestContext ctx = oneOffRequestContext.openAs(adminUser.getAccountId())) {
+
+      return groupMembers
+          .create(adminUser)
+          .listAccounts(group.getGroupUUID(), null)
+          .stream()
+          .map(Account::getId)
+          .collect(Collectors.toSet());
+    } catch (NoSuchGroupException | NoSuchProjectException | OrmException | IOException e) {
+      log.error("Unable to list accounts in group " + group, e);
+      return Collections.emptySet();
+    }
+  }
+
+  private Set<Account.Id> findUserOrEmail(String nameOrEmail) {
     try {
       Set<Id> accountIds = resolver.findAll(db, nameOrEmail);
       if (accountIds.isEmpty()) {
@@ -80,7 +134,11 @@ public class AccountsImpl implements Accounts {
         || account
             .getExternalIds()
             .stream()
-            .anyMatch(extId -> getSchemeRest(extId.key().scheme(), extId.key().get()).trim().equalsIgnoreCase(nameOrEmail));
+            .anyMatch(
+                extId ->
+                    getSchemeRest(extId.key().scheme(), extId.key().get())
+                        .trim()
+                        .equalsIgnoreCase(nameOrEmail));
   }
 
   private String getSchemeRest(String scheme, String key) {
