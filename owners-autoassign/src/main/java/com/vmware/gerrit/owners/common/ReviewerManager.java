94bce1265661cb3731e82fa6d1db662addd6626d
Luca Milanesio <luca.milanesio@gmail.com>
2018-02-28 15:48:06 +0000

    Get rid of references to ReviewDb
    Leverage GerritApi and do not refer to ReviewDb
    anymore. Starting from v2.15 NoteDb is becoming mainstream
    and assumption of being on ReviewDb is not valid anymore.
    
    Change-Id: Ie8f960b00ed3edf2341ed99f159cf75115236447

--- owners-autoassign/src/main/java/com/vmware/gerrit/owners/common/ReviewerManager.java
+++ owners-autoassign/src/main/java/com/vmware/gerrit/owners/common/ReviewerManager.java
@@ -16,12 +16,11 @@
 
 package com.vmware.gerrit.owners.common;
 
-import com.google.gerrit.extensions.api.GerritApi;
 import com.google.gerrit.extensions.api.changes.AddReviewerInput;
+import com.google.gerrit.extensions.api.changes.ChangeApi;
 import com.google.gerrit.extensions.api.changes.ReviewInput;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.reviewdb.client.Account;
-import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.server.util.ManualRequestContext;
 import com.google.gerrit.server.util.OneOffRequestContext;
 import com.google.gwtorm.server.OrmException;
@@ -36,18 +35,17 @@ import org.slf4j.LoggerFactory;
 public class ReviewerManager {
   private static final Logger log = LoggerFactory.getLogger(ReviewerManager.class);
 
-  private final GerritApi gApi;
   private final OneOffRequestContext requestContext;
 
   @Inject
-  public ReviewerManager(GerritApi gApi, OneOffRequestContext requestContext) {
-    this.gApi = gApi;
+  public ReviewerManager(OneOffRequestContext requestContext) {
     this.requestContext = requestContext;
   }
 
-  public void addReviewers(Change change, Collection<Account.Id> reviewers)
+  public void addReviewers(ChangeApi cApi, Collection<Account.Id> reviewers)
       throws ReviewerManagerException {
-    try (ManualRequestContext ctx = requestContext.openAs(change.getOwner())) {
+    try (ManualRequestContext ctx =
+        requestContext.openAs(new Account.Id(cApi.get().owner._accountId))) {
       // TODO(davido): Switch back to using changes API again,
       // when it supports batch mode for adding reviewers
       ReviewInput in = new ReviewInput();
@@ -57,7 +55,6 @@ public class ReviewerManager {
         addReviewerInput.reviewer = account.toString();
         in.reviewers.add(addReviewerInput);
       }
-      gApi.changes().id(change.getId().get()).current().review(in);
     } catch (RestApiException | OrmException e) {
       log.error("Couldn't add reviewers to the change", e);
       throw new ReviewerManagerException(e);
