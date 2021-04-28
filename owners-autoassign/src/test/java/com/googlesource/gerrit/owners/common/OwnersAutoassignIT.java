c1f81ad703fc763b5dd82aafb0fc8e9995457547
Luca Milanesio <luca.milanesio@gmail.com>
2021-04-28 22:35:45 +0100

    Allow custom initial attention-set from owners
    Avoid adding too many reviewers automatically to the attention-set
    when a change is created. The OWNERS list, because of hierarchy, may
    pull a lot of names as reviewers, but that doesn't mean that all of them
    should have the initial task to provide the first feedback.
    
    Expose an API (OwnersAttentionSet that allows an external script
    or plugin to specify the logic for selecting the accounts for the
    attention set based on:
    - Owners
    - Change
    
    The new API is contained in owners-api.jar that needs to be installed
    and configured as a libModule as follows:
    
    [gerrit]
      installModule = com.googlesource.gerrit.owners.api.OwnersApiModule
    
    Implementing the API for adding owners to attention-set is quite easy
    and can be done also through scripting.
    
    In Groovy it would be as easy as:
    
    class MySelector implements OwnersAttentionSet {
      Collection<Account.Id> addToAttentionSet
          (ChangeInfo changeInfo,
           Collection<Account.Id> owners) {
        owners.take(2)
      }
    }
    
    For existing owners-autoassign plugins, the injection of the
    DynamicItem<OwnersAttentionSet> is optional and therefore is fully
    transparent and all owners are added to the attention-set as per
    Gerrit default mechanism.
    
    Bug: Issue 14452
    Change-Id: Ie2a6fbc289c66d99986bbaacb6ea4c9864515513

---
+++ owners-autoassign/src/test/java/com/googlesource/gerrit/owners/common/OwnersAutoassignIT.java
@@ -0,0 +1,64 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.owners.common;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.acceptance.UseLocalDisk;
+import com.google.gerrit.extensions.api.changes.ChangeApi;
+import com.google.gerrit.extensions.client.ReviewerState;
+import com.google.gerrit.extensions.common.AccountInfo;
+import com.google.inject.AbstractModule;
+import java.util.Collection;
+import org.junit.Test;
+
+@TestPlugin(
+    name = "owners-api",
+    sysModule = "com.vmware.gerrit.owners.common.OwnersAutoassignIT$TestModule")
+public class OwnersAutoassignIT extends LightweightPluginDaemonTest {
+
+  public static class TestModule extends AbstractModule {
+    @Override
+    protected void configure() {
+      install(new AutoassignModule());
+    }
+  }
+
+  @UseLocalDisk
+  @Test
+  public void shouldAutoassignOneOwner() throws Exception {
+    String ownerEmail = user.email();
+
+    pushFactory
+        .create(
+            admin.newIdent(),
+            testRepo,
+            "Set OWNERS",
+            "OWNERS",
+            "inherited: false\n" + "owners:\n" + "- " + ownerEmail)
+        .to("refs/heads/master")
+        .assertOkStatus();
+
+    ChangeApi changeApi = change(createChange());
+    Collection<AccountInfo> reviewers = changeApi.get().reviewers.get(ReviewerState.REVIEWER);
+
+    assertThat(reviewers).hasSize(1);
+    AccountInfo reviewer = reviewers.iterator().next();
+    assertThat(reviewer.email).isEqualTo(ownerEmail);
+  }
+}
