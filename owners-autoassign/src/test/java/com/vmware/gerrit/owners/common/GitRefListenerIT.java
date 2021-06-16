e419ee90ee02b5d6f65fedb8d6a71a4606fb1fb0
Luca Milanesio <luca.milanesio@gmail.com>
2021-06-16 21:48:00 +0100

    Trigger owners-autoassign processing for change readyForReview
    The owners-autoassign plugin has historically ignored
    all refs/changes/*/meta ref-updates, because they are related
    to modifications on the change meta-data.
    
    There is one moment, though, where the evaluation make a lot
    of sense, and it is a meta-data only change, triggering only
    the ref-update of the refs/changes/*/meta: a WIP change is
    set ready for review.
    
    Very often, the user that is working on a WIP change does not
    like to have lots of reviewers and therefore tends to remove
    them from the reviewers' list. Also, WIP changes do not generate
    any attention-set action because the change isn't supposed to
    be ready for review.
    
    When the change is later set ready for review *without any
    further updates*, the owners-autoassign must process the change
    and, if there aren't already the expected owners added,
    should add all the owners retrieved from the processing
    of the OWNERS files as reviewers.
    The detection of the diff cannot rely though on the new SHA1
    of the event but rather get the latest patch-set SHA1 and
    compute the diff against its change-base.
    
    Bug: Issue 14679
    Change-Id: I6b2b6f9b947615f96e331709aa257d54b9c52f07

--- owners-autoassign/src/test/java/com/vmware/gerrit/owners/common/GitRefListenerIT.java
+++ owners-autoassign/src/test/java/com/vmware/gerrit/owners/common/GitRefListenerIT.java
@@ -19,13 +19,17 @@ import static org.junit.Assert.assertEquals;
 
 import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
 import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.acceptance.UseLocalDisk;
 import com.google.gerrit.entities.RefNames;
 import com.google.gerrit.extensions.events.GitReferenceUpdatedListener;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.registration.Extension;
 import com.google.gerrit.server.AnonymousUser;
 import com.google.gerrit.server.util.ManualRequestContext;
 import com.google.gerrit.server.util.ThreadLocalRequestContext;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
+import java.util.stream.StreamSupport;
 import org.eclipse.jgit.transport.ReceiveCommand.Type;
 import org.junit.Test;
 
@@ -34,7 +38,7 @@ import org.junit.Test;
     sysModule = "com.vmware.gerrit.owners.common.GitRefListenerIT$TestModule")
 public class GitRefListenerIT extends LightweightPluginDaemonTest {
 
-  @Inject GitRefListenerTest gitRefListener;
+  @Inject DynamicSet<GitReferenceUpdatedListener> allRefUpdateListeners;
   @Inject ThreadLocalRequestContext requestContext;
 
   String aRefChange = RefNames.REFS_CHANGES + "01/01/01";
@@ -44,36 +48,51 @@ public class GitRefListenerIT extends LightweightPluginDaemonTest {
   public static class TestModule extends AbstractModule {
     @Override
     protected void configure() {
-      bind(GitReferenceUpdatedListener.class).to(GitRefListenerTest.class);
+      DynamicSet.bind(binder(), GitReferenceUpdatedListener.class).to(GitRefListenerTest.class);
     }
   }
 
   @Test
-  public void shouldNotProcessNoteDbOnlyRefs() {
+  public void shouldNotProcessNoteDbOnlyRefs() throws Exception {
+    String changeRefPrefix = createChange().getChange().getId().toRefPrefix();
+    int baselineProcessedEvents = gitRefListener().getProcessedEvents();
+
     ReferenceUpdatedEventTest refUpdatedEvent =
         new ReferenceUpdatedEventTest(
             project,
-            RefNames.REFS_CHANGES + "01/01" + RefNames.META_SUFFIX,
+            changeRefPrefix + RefNames.META_SUFFIX.substring(1),
             anOldObjectId,
             aNewObjectId,
             Type.CREATE,
             admin.id());
 
-    gitRefListener.onGitReferenceUpdated(refUpdatedEvent);
-    assertEquals(0, gitRefListener.getProcessedEvents());
+    gitRefListener().onGitReferenceUpdated(refUpdatedEvent);
+    assertEquals(0, gitRefListener().getProcessedEvents() - baselineProcessedEvents);
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shoulProcessSetReadyForReviewOnNoteDb() throws Exception {
+    int wipChangeNum = createChange().getChange().getId().get();
+    gApi.changes().id(wipChangeNum).setWorkInProgress();
+
+    int baselineProcessedEvents = gitRefListener().getProcessedEvents();
+
+    gApi.changes().id(wipChangeNum).setReadyForReview();
+    assertEquals(1, gitRefListener().getProcessedEvents() - baselineProcessedEvents);
   }
 
   @Test
   public void shouldProcessRefChanges() {
-    gitRefListener.onGitReferenceUpdated(newRefUpdateEvent());
-    assertEquals(1, gitRefListener.getProcessedEvents());
+    gitRefListener().onGitReferenceUpdated(newRefUpdateEvent());
+    assertEquals(1, gitRefListener().getProcessedEvents());
   }
 
   @Test
   public void shouldRetrieveChangeFromAnonymousContext() throws Exception {
     try (ManualRequestContext ctx = new ManualRequestContext(new AnonymousUser(), requestContext)) {
-      gitRefListener.onGitReferenceUpdated(newRefUpdateEvent());
-      assertEquals(1, gitRefListener.getProcessedEvents());
+      gitRefListener().onGitReferenceUpdated(newRefUpdateEvent());
+      assertEquals(1, gitRefListener().getProcessedEvents());
     }
   }
 
@@ -83,9 +102,18 @@ public class GitRefListenerIT extends LightweightPluginDaemonTest {
         new ReferenceUpdatedEventTest(
             project, aRefChange, anOldObjectId, aNewObjectId, Type.CREATE, null);
     try (ManualRequestContext ctx = new ManualRequestContext(new AnonymousUser(), requestContext)) {
-      gitRefListener.onGitReferenceUpdated(refUpdateWithoutAccountId);
-      assertEquals(1, gitRefListener.getProcessedEvents());
+      gitRefListener().onGitReferenceUpdated(refUpdateWithoutAccountId);
+      assertEquals(1, gitRefListener().getProcessedEvents());
     }
+  }
+
+  private GitRefListenerTest gitRefListener() {
+    return (GitRefListenerTest)
+        StreamSupport.stream(allRefUpdateListeners.entries().spliterator(), false)
+            .map(Extension::get)
+            .filter(listener -> GitRefListenerTest.class.isAssignableFrom(listener.getClass()))
+            .findFirst()
+            .get();
   }
 
   private ReferenceUpdatedEventTest newRefUpdateEvent() {
