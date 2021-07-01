e3dfc47f91cea7cdff7a32e0b58ddffecc60242d
Antonio Barone <syntonyze@gmail.com>
2021-07-01 16:18:06 +0200

    Only add owners with permissions to attention-set
    When auto-assigning reviewers to the change, only the ones that have
    visibility on the change itself are added, whilst candidate reviewers
    that have no permissions are discarded.
    
    The same logic however was not applied to the attention-set, so that all
    candidate owners were blindly added without first checking whether they
    actually have permission to read the change.
    
    When such situation happens, an exception is thrown that actually
    prevents *all* owners from being added to the attention set.
    
    Only set owners that have read permissions to the attention-set,
    similarly to what is already done for reviewers.
    
    Bug: Issue 14728
    Change-Id: I46f66369b964b48af953ddbc4597ff19d56d6b23

--- owners-autoassign/src/test/java/com/googlesource/gerrit/owners/common/OwnersAutoassignWithAttentionSetIT.java
+++ owners-autoassign/src/test/java/com/googlesource/gerrit/owners/common/OwnersAutoassignWithAttentionSetIT.java
@@ -16,20 +16,30 @@
 package com.googlesource.gerrit.owners.common;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.acceptance.testsuite.project.TestProjectUpdate.block;
 import static java.util.stream.Collectors.toList;
 
 import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.TestAccount;
 import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.acceptance.testsuite.project.ProjectOperations;
+import com.google.gerrit.entities.AccessSection;
 import com.google.gerrit.entities.Account.Id;
+import com.google.gerrit.entities.AccountGroup;
+import com.google.gerrit.entities.Permission;
+import com.google.gerrit.extensions.api.groups.GroupInput;
 import com.google.gerrit.extensions.client.ReviewerState;
 import com.google.gerrit.extensions.common.ChangeInfo;
 import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
 import com.googlesource.gerrit.owners.api.OwnersApiModule;
 import com.googlesource.gerrit.owners.api.OwnersAttentionSet;
 import java.util.Collection;
+import java.util.Collections;
 import org.junit.Test;
 
 @TestPlugin(
@@ -37,6 +47,8 @@ import org.junit.Test;
     sysModule =
         "com.googlesource.gerrit.owners.common.OwnersAutoassignWithAttentionSetIT$TestModule")
 public class OwnersAutoassignWithAttentionSetIT extends LightweightPluginDaemonTest {
+
+  @Inject private ProjectOperations projectOperations;
 
   @Override
   public Module createModule() {
@@ -67,6 +79,35 @@ public class OwnersAutoassignWithAttentionSetIT extends LightweightPluginDaemonT
     String ownerEmail1 = user.email();
     String ownerEmail2 = accountCreator.user2().email();
 
+    setOwners(ownerEmail1, ownerEmail2);
+
+    ChangeInfo change = change(createChange()).get();
+    assertThat(change.reviewers.get(ReviewerState.REVIEWER)).hasSize(2);
+    assertThat(change.attentionSet).hasSize(1);
+  }
+
+  @Test
+  public void shouldAddToAttentionSetOneUserIfAnotherUserHasNoPermission() throws Exception {
+    TestAccount userWithAccessToProject = accountCreator.user();
+    TestAccount userWithNoAccessToProject = accountCreator.user2();
+
+    AccountGroup.UUID groupWithNoAccessToProject =
+        createGroup("groupWithNoAccessToProject", userWithNoAccessToProject);
+
+    setOwners(userWithAccessToProject.email(), userWithNoAccessToProject.email());
+
+    projectOperations
+        .project(project)
+        .forUpdate()
+        .add(block(Permission.READ).ref(AccessSection.ALL).group(groupWithNoAccessToProject))
+        .update();
+
+    ChangeInfo change = change(createChange()).get();
+    assertThat(change.attentionSet).isNotNull();
+    assertThat(change.attentionSet.keySet()).containsExactly(userWithAccessToProject.id().get());
+  }
+
+  private void setOwners(String ownerEmail1, String ownerEmail2) throws Exception {
     pushFactory
         .create(
             admin.newIdent(),
@@ -83,9 +124,12 @@ public class OwnersAutoassignWithAttentionSetIT extends LightweightPluginDaemonT
                 + "\n")
         .to("refs/heads/master")
         .assertOkStatus();
+  }
 
-    ChangeInfo change = change(createChange()).get();
-    assertThat(change.reviewers.get(ReviewerState.REVIEWER)).hasSize(2);
-    assertThat(change.attentionSet).hasSize(1);
+  private AccountGroup.UUID createGroup(String name, TestAccount member) throws RestApiException {
+    GroupInput groupInput = new GroupInput();
+    groupInput.name = name(name);
+    groupInput.members = Collections.singletonList(String.valueOf(member.id().get()));
+    return AccountGroup.uuid(gApi.groups().create(groupInput).get().id);
   }
 }
