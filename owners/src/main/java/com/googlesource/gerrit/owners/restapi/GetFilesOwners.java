81dbf20c7cfa9d5698d20583d7a91b93355ac558
Fabio Ponciroli <ponch78@gmail.com>
2022-09-02 21:14:59 +0200

    REST API to expose owners of each file
    This API will expose, for a given patch set, all the owners
    of a given file.
    
    Here an example:
    
    ```
    GET /changes/{change-id}/revisions/{revision-id}/owners~files-owners
    
    {
      "AJavaFile.java":[
          { "name":"John", "id": 1 },
          { "name":"Bob", "id": 2 }
      ],
      "Aptyhonfileroot.py":[
          { "name":"John", "id": 1 },
          { "name":"Bob", "id": 2 },
          { "name":"John", "id": 3 }
      ]
    }
    ```
    
    The full name and id of the owner will be displayed. If not available
    it will fallback to the account Id.
    
    Change-Id: I4167989fe0fe58efa7e547c06489960af13de73c

---
+++ owners/src/main/java/com/googlesource/gerrit/owners/restapi/GetFilesOwners.java
@@ -0,0 +1,103 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.owners.restapi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Maps;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.entities.Change;
+import com.google.gerrit.entities.PatchSet;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.account.AccountCache;
+import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.change.RevisionResource;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.patch.PatchList;
+import com.google.gerrit.server.patch.PatchListCache;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.owners.common.Accounts;
+import com.googlesource.gerrit.owners.common.PathOwners;
+import com.googlesource.gerrit.owners.common.PluginSettings;
+import com.googlesource.gerrit.owners.entities.Owner;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.eclipse.jgit.lib.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class GetFilesOwners implements RestReadView<RevisionResource> {
+  private static final Logger log = LoggerFactory.getLogger(GetFilesOwners.class);
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  private final PatchListCache patchListCache;
+  private final Accounts accounts;
+  private final AccountCache accountCache;
+  private final GitRepositoryManager repositoryManager;
+  private final PluginSettings pluginSettings;
+
+  @Inject
+  GetFilesOwners(
+      PatchListCache patchListCache,
+      Accounts accounts,
+      AccountCache accountCache,
+      GitRepositoryManager repositoryManager,
+      PluginSettings pluginSettings) {
+    this.patchListCache = patchListCache;
+    this.accounts = accounts;
+    this.accountCache = accountCache;
+    this.repositoryManager = repositoryManager;
+    this.pluginSettings = pluginSettings;
+  }
+
+  @Override
+  public Response<?> apply(RevisionResource revision)
+      throws AuthException, BadRequestException, ResourceConflictException, Exception {
+    PatchSet ps = revision.getPatchSet();
+    Change change = revision.getChange();
+
+    try (Repository repository = repositoryManager.openRepository(change.getProject())) {
+      PatchList patchList = patchListCache.get(change, ps);
+
+      String branch = change.getDest().branch();
+      PathOwners owners =
+          new PathOwners(
+              accounts,
+              repository,
+              pluginSettings.isBranchDisabled(branch) ? Optional.empty() : Optional.of(branch),
+              patchList);
+
+      Map<String, Set<Owner>> fileToOwners =
+          Maps.transformValues(
+              owners.getFileOwners(),
+              ids -> ids.stream().map(this::getOwnerFromAccountId).collect(Collectors.toSet()));
+
+      return Response.ok(fileToOwners);
+    }
+  }
+
+  private Owner getOwnerFromAccountId(Account.Id accountId) {
+    AccountState accountState = accountCache.getEvenIfMissing(accountId);
+    return new Owner(accountState.account().fullName(), accountState.account().id().get());
+  }
+}
---
+++ owners/src/test/java/com/googlesource/gerrit/owners/restapi/GetFilesOwnersIT.java
@@ -0,0 +1,98 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.owners.restapi;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.common.collect.Iterables;
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.acceptance.UseLocalDisk;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.server.change.RevisionResource;
+import com.googlesource.gerrit.owners.common.OwnersConfig;
+import com.googlesource.gerrit.owners.entities.Owner;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.compress.utils.Sets;
+import org.junit.Test;
+
+@TestPlugin(name = "owners", httpModule = "com.googlesource.gerrit.owners.OwnersRestApiModule")
+public class GetFilesOwnersIT extends LightweightPluginDaemonTest {
+
+  private GetFilesOwners ownersApi;
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @Override
+  public void setUpTestPlugin() throws Exception {
+    super.setUpTestPlugin();
+
+    ownersApi = plugin.getSysInjector().getInstance(GetFilesOwners.class);
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldReturnACorrectResponse() throws Exception {
+    // Add OWNERS file to root:
+    //
+    // inherited: true
+    // owners:
+    // - Administrator
+    merge(createChange(testRepo, "master", "Add OWNER file", "OWNERS", getOwnerFileContent(), ""));
+
+    PushOneCommit.Result result = createChange();
+    RevisionResource revisionResource = parseCurrentRevisionResource(result.getChangeId());
+
+    Response<?> resp = ownersApi.apply(revisionResource);
+
+    assertThat(resp.statusCode()).isEqualTo(HttpServletResponse.SC_OK);
+
+    Map<String, Set<Owner>> responseValue = (Map<String, Set<Owner>>) resp.value();
+    HashMap<String, Set<Owner>> expectedResponseValue =
+        new HashMap<String, Set<Owner>>() {
+          {
+            put("a.txt", Sets.newHashSet(new Owner(admin.fullName(), admin.id().get())));
+          }
+        };
+
+    assertThat(responseValue.keySet()).isEqualTo(expectedResponseValue.keySet());
+
+    final Collection<Set<Owner>> owners = responseValue.values();
+    final Collection<Set<Owner>> expectedOwners = expectedResponseValue.values();
+    assertThat(owners.size()).isEqualTo(expectedOwners.size());
+
+    Owner owner = Iterables.getFirst(owners.stream().findFirst().get(), null);
+    assertThat(owner.getId()).isEqualTo(admin.id().get());
+    assertThat(owner.getName()).isEqualTo(admin.fullName());
+  }
+
+  private String getOwnerFileContent() throws JsonProcessingException {
+    OwnersConfig ownersConfig = new OwnersConfig();
+    ownersConfig.setOwners(Collections.singleton(admin.email()));
+    ObjectMapper om = new ObjectMapper(new YAMLFactory());
+
+    return om.writeValueAsString(ownersConfig);
+  }
+}
