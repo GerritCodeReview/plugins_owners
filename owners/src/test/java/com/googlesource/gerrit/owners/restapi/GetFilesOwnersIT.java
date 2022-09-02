45c4ca44cf9c1b16babad4fc8ffc62a1d3e569e0
Fabio Ponciroli <ponch78@gmail.com>
2022-09-02 21:14:59 +0200

    REST API to expose owners of each file
    This API will expose, for a given patch set, all the owners
    of a given file.
    
    Here an example:
    
    ```
    GET /changes/{change-id}/revisions/{revision-id}/owners~files-owners
    
    {
      "AJavaFile.java":[ { "name":"John", "id": 1 }, { "name":"Bob", "id": 2 }],
      "Aptyhonfileroot.py":[{ "name":"John", "id": 1 }, { "name":"Bob", "id": 2 }, { "name":"John", "id": 3 }]}
    }
    ```
    
    The full name and id of the owner will be displayed. If not available it will
    fallback to the account Id.
    
    Change-Id: I4167989fe0fe58efa7e547c06489960af13de73c

---
+++ owners/src/test/java/com/googlesource/gerrit/owners/restapi/GetFilesOwnersIT.java
@@ -0,0 +1,91 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.owners.restapi;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.acceptance.UseLocalDisk;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.server.change.RevisionResource;
+import com.googlesource.gerrit.owners.common.OwnersConfig;
+import com.googlesource.gerrit.owners.entities.Owner;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.Test;
+
+@TestPlugin(name = "owners", httpModule = "com.googlesource.gerrit.owners.OwnersRestApiModule")
+public class GetFilesOwnersIT extends LightweightPluginDaemonTest {
+
+  GetFilesOwners ownersApi;
+
+  @Override
+  public void setUpTestPlugin() throws Exception {
+    super.setUpTestPlugin();
+
+    ownersApi = plugin.getSysInjector().getInstance(GetFilesOwners.class);
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldReturnACorrectResponse() throws Exception {
+    // Add OWNERS file to root:
+    //
+    // inherited: true
+    // owners:
+    // - Administrator
+    PushOneCommit.Result r =
+        createChange(testRepo, "master", "x", "OWNERS", getOwnerFileContent(), "");
+    merge(r);
+
+    PushOneCommit.Result r2 = createChange();
+    RevisionResource revisionResource = parseCurrentRevisionResource(r2.getChangeId());
+
+    Response<?> resp = ownersApi.apply(revisionResource);
+
+    assertThat(resp.statusCode()).isEqualTo(HttpServletResponse.SC_OK);
+    assertThat(resp.value()).isEqualTo(getExpectedResponse());
+  }
+
+  private String getOwnerFileContent() throws JsonProcessingException {
+    OwnersConfig ownersConfig = new OwnersConfig();
+    ownersConfig.setOwners(Collections.singleton(admin.email()));
+    ObjectMapper om = new ObjectMapper(new YAMLFactory());
+
+    return om.writeValueAsString(ownersConfig);
+  }
+
+  private String getExpectedResponse() throws JsonProcessingException {
+    HashMap<String, List<Owner>> expectedResponse =
+        new HashMap<String, List<Owner>>() {
+          {
+            put(
+                "a.txt",
+                Collections.singletonList(new Owner(admin.fullName(), admin.id().toString())));
+          }
+        };
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    return objectMapper.writeValueAsString(expectedResponse);
+  }
+}
