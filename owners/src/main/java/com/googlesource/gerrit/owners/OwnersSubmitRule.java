342d8814d1abd8c1e079cbd4d5901f82e98b8a25
Jacek Centkowski <geminica.programs@gmail.com>
2022-12-20 07:57:45 +0100

    Get file owners for a change in question
    For the time being obtain file owners for a given change and log if
    none is present.
    TODO: check if change is approved
    
    Bug: Issue 15556
    Change-Id: I6c1e13c232c28fa3fe0a234da79fe1b201686ef4

--- owners/src/main/java/com/googlesource/gerrit/owners/OwnersSubmitRule.java
+++ owners/src/main/java/com/googlesource/gerrit/owners/OwnersSubmitRule.java
@@ -18,8 +18,13 @@ import static com.google.gerrit.server.project.ProjectCache.noSuchProject;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Project;
 import com.google.gerrit.entities.SubmitRecord;
 import com.google.gerrit.extensions.annotations.Exports;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.patch.DiffNotAvailableException;
+import com.google.gerrit.server.patch.DiffOperations;
+import com.google.gerrit.server.patch.filediff.FileDiffOutput;
 import com.google.gerrit.server.project.NoSuchProjectException;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.gerrit.server.project.ProjectState;
@@ -28,8 +33,17 @@ import com.google.gerrit.server.rules.SubmitRule;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.googlesource.gerrit.owners.common.Accounts;
+import com.googlesource.gerrit.owners.common.PathOwners;
 import com.googlesource.gerrit.owners.common.PluginSettings;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
 
 @Singleton
 public class OwnersSubmitRule implements SubmitRule {
@@ -46,11 +60,22 @@ public class OwnersSubmitRule implements SubmitRule {
 
   private final PluginSettings pluginSettings;
   private final ProjectCache projectCache;
+  private final Accounts accounts;
+  private final GitRepositoryManager repoManager;
+  private final DiffOperations diffOperations;
 
   @Inject
-  OwnersSubmitRule(PluginSettings pluginSettings, ProjectCache projectCache) {
+  OwnersSubmitRule(
+      PluginSettings pluginSettings,
+      ProjectCache projectCache,
+      Accounts accounts,
+      GitRepositoryManager repoManager,
+      DiffOperations diffOperations) {
     this.pluginSettings = pluginSettings;
     this.projectCache = projectCache;
+    this.accounts = accounts;
+    this.repoManager = repoManager;
+    this.diffOperations = diffOperations;
   }
 
   @Override
@@ -75,11 +100,51 @@ public class OwnersSubmitRule implements SubmitRule {
             "Project has prolog rules enabled. It may interfere with submit rule evaluation.");
       }
 
-      logger.atInfo().log("TODO: evaluate OWNERS file based rules if available");
+      String branch = cd.change().getDest().branch();
+      List<Project.NameKey> parents =
+          Optional.ofNullable(projectState.getProject().getParent())
+              .map(Arrays::asList)
+              .orElse(Collections.emptyList());
+
+      try (Repository repo = repoManager.openRepository(cd.project())) {
+        PathOwners pathOwners =
+            new PathOwners(
+                accounts,
+                repoManager,
+                repo,
+                parents,
+                pluginSettings.isBranchDisabled(branch) ? Optional.empty() : Optional.of(branch),
+                getDiff(cd.project(), cd.currentPatchSet().commitId()),
+                pluginSettings.expandGroups());
+
+        if (pathOwners.getFileOwners().isEmpty()) {
+          logger.atInfo().log("Change has no file owners defined. Skipping rules.");
+          return Optional.empty();
+        }
+
+        logger.atInfo().log("TODO: check if change is approved.");
+      } catch (IOException | DiffNotAvailableException e) {
+        logger.atSevere().withCause(e).log("Opening repository failed");
+      }
     } catch (NoSuchProjectException e) {
+      logger.atInfo().log("TODO: handle exceptions");
       throw new IllegalStateException("Unable to find project while evaluating owners rule", e);
     }
 
     return Optional.empty();
+  }
+
+  private Map<String, FileDiffOutput> getDiff(Project.NameKey project, ObjectId revision)
+      throws DiffNotAvailableException {
+    requireNonNull(project, "project");
+    requireNonNull(revision, "revision");
+
+    // Use parentNum=0 to do the comparison against the default base.
+    // For non-merge commits the default base is the only parent (aka parent 1, initial commits
+    // are not supported).
+    // For merge commits the default base is the auto-merge commit which should be used as base IOW
+    // only the changes from it should be reviewed as changes against the parent 1 were already
+    // reviewed
+    return diffOperations.listModifiedFilesAgainstParent(project, revision, 0);
   }
 }
