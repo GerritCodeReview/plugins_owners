05971bdf1210f029f7a094b557d7e490e073cf51
Jacek Centkowski <geminica.programs@gmail.com>
2024-10-29 23:51:34 +0100

    Display file owner's vote value in the File Owners summary
    Display the vote value of a label that is required in order to satisfy
    the OWNERS definition as returned from the plugin's API (typically
    `Code-Review`). Note that default or different (e.g. `Verified`) labels
    are not shown as they are not relevant from the owners perspective.
    
    The `computeApprovalAndInfo` function (that is responsible for getting
    the necessary information) is covered in the unit tests.
    
    Note that a dedicated UI component (`gr-owner`) was introduced to
    encapsulate the owner's details. It has to be a part of `gr-owners` file
    as otherwise it is not properly loaded.
    
    TODO: add copy file owner's email button
    
    Bug: Issue 373151160
    Change-Id: I07919c66f4cda95da40a62086a1b832aec7e8a4a

--- owners/web/gr-owners_test.ts
+++ owners/web/gr-owners_test.ts
@@ -17,14 +17,21 @@
 
 import {assert} from '@open-wc/testing';
 
-import {getFileOwnership, shouldHide} from './gr-owners';
+import {
+  computeApprovalAndInfo,
+  getFileOwnership,
+  shouldHide,
+} from './gr-owners';
 import {FileOwnership, FileStatus, PatchRange, UserRole} from './owners-model';
 import {
+  AccountInfo,
+  ApprovalInfo,
   ChangeInfo,
   ChangeStatus,
+  DetailedLabelInfo,
   SubmitRequirementResultInfo,
 } from '@gerritcodereview/typescript-api/rest-api';
-import {FilesOwners} from './owners-service';
+import {FilesOwners, OwnersLabels} from './owners-service';
 import {deepEqual} from './utils';
 
 suite('owners status tests', () => {
@@ -245,6 +252,101 @@ suite('owners status tests', () => {
             fileStatus: FileStatus.NEEDS_APPROVAL,
             owners: [{name: 'John', id: 1}],
           } as FileOwnership
+        ),
+        true
+      );
+    });
+  });
+
+  suite('computeApprovalAndInfo tests', () => {
+    const account = 1;
+    const fileOwner = {_account_id: account} as unknown as AccountInfo;
+    const label = 'Code-Review';
+    const crPlus1OwnersVote = {
+      [`${account}`]: {[label]: 1},
+    } as unknown as OwnersLabels;
+    const changeWithLabels = {
+      labels: {
+        [label]: {
+          all: [
+            {
+              value: 1,
+              date: '2024-10-22 17:26:21.000000000',
+              permitted_voting_range: {
+                min: -2,
+                max: 2,
+              },
+              _account_id: account,
+            },
+          ],
+          values: {
+            '-2': 'This shall not be submitted',
+            '-1': 'I would prefer this is not submitted as is',
+            ' 0': 'No score',
+            '+1': 'Looks good to me, but someone else must approve',
+            '+2': 'Looks good to me, approved',
+          },
+          description: '',
+          default_value: 0,
+        },
+      },
+    } as unknown as ChangeInfo;
+
+    test('computeApprovalAndInfo - should be `undefined` when change is `undefined', () => {
+      const undefinedChange = undefined;
+      assert.equal(
+        computeApprovalAndInfo(fileOwner, crPlus1OwnersVote, undefinedChange),
+        undefined
+      );
+    });
+
+    test('computeApprovalAndInfo - should be `undefined` when there is no owners vote', () => {
+      const emptyOwnersVote = {};
+      assert.equal(
+        computeApprovalAndInfo(fileOwner, emptyOwnersVote, changeWithLabels),
+        undefined
+      );
+    });
+
+    test('computeApprovalAndInfo - should be `undefined` for default owners vote', () => {
+      const defaultOwnersVote = {[label]: 0} as unknown as OwnersLabels;
+      assert.equal(
+        computeApprovalAndInfo(fileOwner, defaultOwnersVote, changeWithLabels),
+        undefined
+      );
+    });
+
+    test('computeApprovalAndInfo - should be computed for CR+1 owners vote', () => {
+      const expectedApproval = {
+        value: 1,
+        date: '2024-10-22 17:26:21.000000000',
+        permitted_voting_range: {
+          min: -2,
+          max: 2,
+        },
+        _account_id: account,
+      } as unknown as ApprovalInfo;
+      const expectedInfo = {
+        all: [expectedApproval],
+        values: {
+          '-2': 'This shall not be submitted',
+          '-1': 'I would prefer this is not submitted as is',
+          ' 0': 'No score',
+          '+1': 'Looks good to me, but someone else must approve',
+          '+2': 'Looks good to me, approved',
+        },
+        description: '',
+        default_value: 0,
+      } as unknown as DetailedLabelInfo;
+
+      assert.equal(
+        deepEqual(
+          computeApprovalAndInfo(
+            fileOwner,
+            crPlus1OwnersVote,
+            changeWithLabels
+          ),
+          [expectedApproval, expectedInfo]
         ),
         true
       );
