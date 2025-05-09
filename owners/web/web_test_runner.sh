c91797c0880079316b83ef72ff0955dfe9de25ac
Paladox none <thomasmulhall410@yahoo.com>
2025-05-09 10:39:19 -0700

    Fix path to tsconfig in web_test_runner.sh
    Change-Id: I14406c2b28dec69bf933dd766cda68952c4f8271

--- owners/web/web_test_runner.sh
+++ owners/web/web_test_runner.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 ./$1 --config $2 \
-  --dir 'plugins/owners/owners/web/_bazel_ts_out_tests' \
-  --test-files 'plugins/owners/owners/web/_bazel_ts_out_tests/*_test.js' \
-  --ts-config="plugins/owners/owners/web/tsconfig.json"
+  --dir 'plugins/owners/web/_bazel_ts_out_tests' \
+  --test-files 'plugins/owners/web/_bazel_ts_out_tests/*_test.js' \
+  --ts-config="plugins/owners/web/tsconfig.json"
