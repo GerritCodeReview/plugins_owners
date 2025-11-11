934fe44e9e798029eb4437586608a807627e6aee
Antonio Barone <syntonyze@gmail.com>
2025-11-11 19:42:46 +0100

    Add predicate for sticky owner approvals
    Introduce a new approval query operand, `already-approved-by_owners`,
    that allows copying owner approvals across patch sets only when it is
    safe to do so.
    
    `AlreadyApprovedByOwnerOperand` registers a `UserInOperandFactory` for
    the new operand, and `AlreadyApprovedByOwnerPredicate` implements the
    matching logic. When evaluating a copy condition for an approval, the
    predicate:
    
    - Computes the set of files modified between the source and target
      patch sets using `DiffOperations`.
    - Checks whether any of the newly modified files are owned by the
      current approver. If so, the approval is not copied and the owner
      must re-approve.
    - If none of the modified files are owned by the approver, copies the
      approval only when the approver has owned at least one file in the
      change at some point.
    
    To support this, `GetFilesOwners` is extended with an `isAnyFileOwnedBy`
    helper that computes ownership for a given set of paths, and the
    construction of `PathOwners` is factored into a private `getPathOwners`
    method reused by both the REST endpoint and the new predicate.
    
    Finally, `OwnersModule` is updated to install the new operand module so
    that the `alreadyApprovedBy` operand is available to approval query
    expressions.
    
    Bug: Issue 458084508
    Change-Id: I1fb57d70756e1a04cae19475afe80b2a329a4d11

---
+++ owners/src/main/java/com/googlesource/gerrit/owners/AlreadyApprovedByOwnerPredicate.java
@@ -0,0 +1,139 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.owners;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.googlesource.gerrit.owners.OwnersApprovalHasOperand.OPERAND;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.entities.PatchSet;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.exceptions.StorageException;
+import com.google.gerrit.index.query.Matchable;
+import com.google.gerrit.index.query.OperatorPredicate;
+import com.google.gerrit.server.git.InMemoryInserter;
+import com.google.gerrit.server.patch.DiffOperations;
+import com.google.gerrit.server.patch.gitdiff.ModifiedFile;
+import com.google.gerrit.server.query.approval.ApprovalContext;
+import com.googlesource.gerrit.owners.restapi.GetFilesOwners;
+import java.io.IOException;
+import java.util.Map;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectInserter;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+public class AlreadyApprovedByOwnerPredicate extends OperatorPredicate<ApprovalContext>
+    implements Matchable<ApprovalContext> {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private final GetFilesOwners getFilesOwners;
+  private final DiffOperations diffOperations;
+
+  public AlreadyApprovedByOwnerPredicate(
+      GetFilesOwners getFilesOwners, DiffOperations diffOperations) {
+    super("approverin", OPERAND);
+    this.getFilesOwners = getFilesOwners;
+    this.diffOperations = diffOperations;
+  }
+
+  @Override
+  public boolean match(ApprovalContext ctx) {
+    try {
+      Account.Id currentApprover = ctx.approverId();
+      Project.NameKey project = ctx.changeData().project();
+      PatchSet targetPatchSet = ctx.targetPatchSet();
+      PatchSet sourcePatchSet = ctx.changeNotes().getPatchSets().get(ctx.sourcePatchSetId());
+
+      checkState(
+          sourcePatchSet != null,
+          "Could not get source patch-set for %s for project %s",
+          ctx.sourcePatchSetId(),
+          project);
+
+      Map<String, ModifiedFile> priorVsCurrent =
+          diffOperations.loadModifiedFilesIfNecessary(
+              project,
+              sourcePatchSet.commitId(),
+              targetPatchSet.commitId(),
+              ctx.repoView().getRevWalk(),
+              ctx.repoView().getConfig(),
+              /* enableRenameDetection= */ false);
+
+      boolean newPatchSetHasFilesOwnedByMe =
+          getFilesOwners.isAnyFileOwnedBy(
+              currentApprover,
+              priorVsCurrent.keySet(),
+              project,
+              ctx.changeData().branchOrThrow().branch());
+
+      if (newPatchSetHasFilesOwnedByMe) {
+        logger.atFinest().log(
+            "Approver '%s' owns files that were changed in this new patch set, must re-approve",
+            currentApprover);
+        return false;
+      }
+
+      // The new patchSet has not modified anything I own.
+      // I will copy my label, but only if I used to own something in the change.
+      try (ObjectInserter ins =
+          new InMemoryInserter(ctx.repoView().getRevWalk().getObjectReader())) {
+
+        Map<String, ModifiedFile> baseVsPrior =
+            diffOperations.loadModifiedFilesAgainstParentIfNecessary(
+                project,
+                sourcePatchSet.commitId(),
+                getParentNum(targetPatchSet.commitId(), ctx.repoView().getRevWalk()),
+                ctx.repoView(),
+                ins,
+                /* enableRenameDetection= */ false);
+        boolean oldPatchSetHasFilesOwnedByMe =
+            getFilesOwners.isAnyFileOwnedBy(
+                currentApprover,
+                baseVsPrior.keySet(),
+                project,
+                ctx.changeData().branchOrThrow().branch());
+
+        logger.atFinest().log(
+            "Has approver '%s' ever owned anything in this change? %s",
+            currentApprover,
+            oldPatchSetHasFilesOwnedByMe
+                ? "yes, will copy approval"
+                : "No, will not copy approval");
+
+        return oldPatchSetHasFilesOwnedByMe;
+      }
+    } catch (Exception e) {
+      logger.atSevere().withCause(e).log("Could not compute %s owner copy condition", OPERAND);
+      return false;
+    }
+  }
+
+  private int getParentNum(ObjectId objectId, RevWalk revWalk) {
+    try {
+      RevCommit commit = revWalk.parseCommit(objectId);
+      // merge commit with 2 or more parents: must use parentNum = 1 to compare against the first
+      // parent. Using parentNum = 0 would compare against the auto-merge.
+      return commit.getParentCount() > 0 ? 1 : 0;
+    } catch (IOException ex) {
+      throw new StorageException(ex);
+    }
+  }
+
+  @Override
+  public int getCost() {
+    return 1;
+  }
+}
