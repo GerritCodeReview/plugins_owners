af4eacf6517c5aeaff8f46844d99b7501fa201bc
Antonio Barone <syntonyze@gmail.com>
2025-11-11 19:42:46 +0100

    Add predicate for sticky owner approvals
    Introduce a new approval query operand, `already-approved-by_owners`,
    that allows copying owner approvals across patch sets only when it is
    safe to do so.
    
    `AlreadyApprovedByOwnerOperand` registers a `UserInOperandFactory` for
    the new operand, and `AlreadyApprovedByOwnerPredicate` implements the
    matching logic. When evaluating a copy condition for an approval, the
    predicate:
    
    - Computes the set of files modified between the source and target
      patch sets using `DiffOperations`.
    - Checks whether any of the newly modified files are owned by the
      current approver. If so, the approval is not copied and the owner
      must re-approve.
    - If none of the modified files are owned by the approver, copies the
      approval only when the approver has owned at least one file in the
      change at some point.
    
    To support this, `GetFilesOwners` is extended with an `isAnyFileOwnedBy`
    helper that computes ownership for a given set of paths, and the
    construction of `PathOwners` is factored into a private `getPathOwners`
    method reused by both the REST endpoint and the new predicate.
    
    Finally, `OwnersModule` is updated to install the new operand module so
    that the `alreadyApprovedBy` operand is available to approval query
    expressions.
    
    Bug: Issue 458084508
    Change-Id: I1fb57d70756e1a04cae19475afe80b2a329a4d11

---
+++ owners/src/main/java/com/googlesource/gerrit/owners/AlreadyApprovedByOwnerOperand.java
@@ -0,0 +1,55 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.owners;
+
+import com.google.gerrit.extensions.annotations.Exports;
+import com.google.gerrit.index.query.Predicate;
+import com.google.gerrit.index.query.QueryParseException;
+import com.google.gerrit.server.patch.DiffOperations;
+import com.google.gerrit.server.query.approval.ApprovalContext;
+import com.google.gerrit.server.query.approval.ApprovalQueryBuilder.UserInOperandFactory;
+import com.google.gerrit.server.query.approval.UserInPredicate;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.owners.restapi.GetFilesOwners;
+
+@Singleton
+public class AlreadyApprovedByOwnerOperand implements UserInOperandFactory {
+  public static final String OPERAND = "already-approved-by";
+  private final GetFilesOwners getFilesOwners;
+  private final DiffOperations diffOperations;
+
+  public static class Module extends AbstractModule {
+
+    @Override
+    protected void configure() {
+      bind(UserInOperandFactory.class)
+          .annotatedWith(Exports.named(OPERAND))
+          .to(AlreadyApprovedByOwnerOperand.class);
+    }
+  }
+
+  @Inject
+  AlreadyApprovedByOwnerOperand(GetFilesOwners getFilesOwners, DiffOperations diffOperations) {
+    this.getFilesOwners = getFilesOwners;
+    this.diffOperations = diffOperations;
+  }
+
+  @Override
+  public Predicate<ApprovalContext> create(UserInPredicate.Field field) throws QueryParseException {
+    return new AlreadyApprovedByOwnerPredicate(getFilesOwners, diffOperations);
+  }
+}
